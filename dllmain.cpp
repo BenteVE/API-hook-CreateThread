5deccec2629e5076fce148453a34857a1767b41c
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-01-30 21:59:46 +0100

    Formatting

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,60 +10,60 @@ True_WriteFile true_Writefile = NULL;
 // Our intercept function
 BOOL WINAPI HookedWriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped)
 {
-    const char* pBuf = "Your original text was replaced by the hooked function!";
-    return true_Writefile(hFile, pBuf, 55, lpNumberOfBytesWritten, lpOverlapped);
+	const char* pBuf = "Your original text was replaced by the hooked function!";
+	return true_Writefile(hFile, pBuf, strlen(pBuf), lpNumberOfBytesWritten, lpOverlapped);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call) {
-        case DLL_PROCESS_ATTACH:
-        {
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH:
+	{
 
-            HMODULE modKernel32 = GetModuleHandle(TEXT("kernel32.dll"));
+		HMODULE modKernel32 = GetModuleHandle(TEXT("kernel32.dll"));
 
-            if (modKernel32 == 0) {
-                MessageBox(HWND_DESKTOP, L"No handle found for kernel32.dll", L"Module handle not found", MB_OK);
-            }
-            else
-            {
-                true_Writefile = (True_WriteFile)GetProcAddress(modKernel32, "WriteFile");
-            }
+		if (modKernel32 == 0) {
+			MessageBox(HWND_DESKTOP, TEXT("No handle found for kernel32.dll"), TEXT("Module handle not found"), MB_OK);
+		}
+		else
+		{
+			true_Writefile = (True_WriteFile)GetProcAddress(modKernel32, "WriteFile");
+		}
 
-            if (true_Writefile == NULL) {
-                MessageBox(HWND_DESKTOP, L"GetProcAddress Failed", L"GetProcAddress Failed, aborting detours", MB_OK);
-            }
-            else {
+		if (true_Writefile == NULL) {
+			MessageBox(HWND_DESKTOP, TEXT("GetProcAddress failed, aborting detours"), TEXT("GetProcAddress Failed"), MB_OK);
+		}
+		else {
 
-                DetourTransactionBegin();
-                DetourUpdateThread(GetCurrentThread());
-                DetourAttach(&(PVOID&)true_Writefile, HookedWriteFile);
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourAttach(&(PVOID&)true_Writefile, HookedWriteFile);
 
-                LONG lError = DetourTransactionCommit();
-                if (lError != NO_ERROR) {
-                    MessageBox(HWND_DESKTOP, L"Failed to detour", L"ATTACH FAILED", MB_OK);
-                    return FALSE;
-                }
-            }
-        }
-        break;
+			LONG lError = DetourTransactionCommit();
+			if (lError != NO_ERROR) {
+				MessageBox(HWND_DESKTOP, TEXT("Failed to attach the hook"), TEXT("Detours Error"), MB_OK);
+				return FALSE;
+			}
+		}
+	}
+	break;
 
-        case DLL_PROCESS_DETACH:
-        {
-            DetourTransactionBegin();
-            DetourUpdateThread(GetCurrentThread());
-            DetourDetach(&(PVOID&)true_Writefile, HookedWriteFile);
+	case DLL_PROCESS_DETACH:
+	{
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		DetourDetach(&(PVOID&)true_Writefile, HookedWriteFile);
 
-            LONG lError = DetourTransactionCommit();
-            if (lError != NO_ERROR) {
-                MessageBox(HWND_DESKTOP, L"Failed to detour", L"DETACH FAILED", MB_OK);
-                return FALSE;
-            }
-        }
-    break;
-    }
+		LONG lError = DetourTransactionCommit();
+		if (lError != NO_ERROR) {
+			MessageBox(HWND_DESKTOP, L"Failed to detach the hook", L"Detours Error", MB_OK);
+			return FALSE;
+		}
+	}
+	break;
+	}
 
-    return TRUE;
+	return TRUE;
 }
 
 
