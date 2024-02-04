1801f8acad7c3bdad8583bde71615075e427ffc0
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-02-04 21:05:48 +0100

    Added console opening to project

---
+++ Console.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+
+class Console {
+public:
+	// Pointer to the opened stream
+	FILE* stream;
+
+	Console();
+	~Console();
+
+	BOOL open();
+	void log(const char*);
+};
