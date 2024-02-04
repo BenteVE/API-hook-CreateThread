2ca2f7656c60e32ab62dbd5023fc843c5e7c2a9e
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-02-04 21:35:29 +0100

    Created hooks on CreateThread and NtCreateThread

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,69 +1,143 @@
 #include <Windows.h>
 
 #include "detours.h"
+#include "Console.h"
 
-// Address of the real WriteFile API
-typedef BOOL(WINAPI* True_WriteFile)(HANDLE, LPCVOID, DWORD, LPDWORD, LPOVERLAPPED);
+Console console;
 
-True_WriteFile true_Writefile = NULL;
+typedef struct _INITIAL_TEB {
+	PVOID                StackBase;
+	PVOID                StackLimit;
+	PVOID                StackCommit;
+	PVOID                StackCommitMax;
+	PVOID                StackReserved;
+} INITIAL_TEB, * PINITIAL_TEB;
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _CLIENT_ID {
+	HANDLE UniqueProcess;
+	HANDLE UniqueThread;
+} CLIENT_ID, * PCLIENT_ID;
+
+typedef struct _OBJECT_ATTRIBUTES {
+	ULONG           Length;
+	HANDLE          RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG           Attributes;
+	PVOID           SecurityDescriptor;
+	PVOID           SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+
+/* signature of NtCreateThread
+NTSYSAPI
+NTSTATUS
+NTAPI
+NtCreateThread(
+	OUT PHANDLE ThreadHandle,
+	IN ACCESS_MASK DesiredAccess,
+	IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
+	IN HANDLE ProcessHandle,
+	OUT PCLIENT_ID ClientId,
+	IN PCONTEXT ThreadContext,
+	IN PINITIAL_TEB InitialTeb,
+	IN BOOLEAN CreateSuspended);
+*/
+
+// Signature of the real NtCreateThread API
+typedef NTSYSAPI NTSTATUS (NTAPI* True_NtCreateThread)(PHANDLE, ACCESS_MASK, POBJECT_ATTRIBUTES, HANDLE, PCLIENT_ID, PCONTEXT, PINITIAL_TEB, BOOLEAN);
+
+True_NtCreateThread true_NtCreateThread = NULL;
 
 // Our intercept function
-BOOL WINAPI HookedWriteFile(HANDLE hFile, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped)
+NTSTATUS NTAPI HookedNtCreateThread(PHANDLE ThreadHandle, ACCESS_MASK DesiredAccess, POBJECT_ATTRIBUTES ObjectAttributes, HANDLE ProcessHandle, PCLIENT_ID ClientId, PCONTEXT ThreadContext, PINITIAL_TEB InitialTeb, BOOLEAN CreateSuspended)
 {
-	const char* pBuf = "Your original text was replaced by the hooked function!";
-	return true_Writefile(hFile, pBuf, strlen(pBuf), lpNumberOfBytesWritten, lpOverlapped);
+	//printf("Created thread with id %i\n", *lpThreadId);
+	NTSTATUS s = true_NtCreateThread(ThreadHandle, DesiredAccess, ObjectAttributes, ProcessHandle, ClientId, ThreadContext, InitialTeb, CreateSuspended);
+	fprintf(console.stream, "Created Nt thread with handle: %p \n", *ThreadHandle);
+
+	return s;
 }
+
+
+
+// Address of the real CreateThread API
+HANDLE (WINAPI* trueCreateThread)(LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, __drv_aliasesMem LPVOID, DWORD, LPDWORD) = CreateThread;
+
+// Our intercept function
+HANDLE WINAPI HookedCreateThread(LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize, LPTHREAD_START_ROUTINE lpStartAddress, __drv_aliasesMem LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId)
+{
+	//printf("Created thread with id %i\n", *lpThreadId);
+	HANDLE h = trueCreateThread(lpThreadAttributes, dwStackSize, lpStartAddress, lpParameter, dwCreationFlags, lpThreadId);
+	fprintf(console.stream, "Created thread with handle: %p \n", h);
+
+	return h;
+}
+
+
+
+
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
+
 	{
-
-		HMODULE modKernel32 = GetModuleHandle(TEXT("kernel32.dll"));
-
-		if (modKernel32 == 0) {
-			MessageBox(HWND_DESKTOP, TEXT("No handle found for kernel32.dll"), TEXT("Module handle not found"), MB_OK);
-		}
-		else
-		{
-			true_Writefile = (True_WriteFile)GetProcAddress(modKernel32, "WriteFile");
+		if (!console.open()) {
+			// Indicate DLL loading failed
+			return FALSE;
 		}
 
-		if (true_Writefile == NULL) {
-			MessageBox(HWND_DESKTOP, TEXT("GetProcAddress failed, aborting detours"), TEXT("GetProcAddress Failed"), MB_OK);
-		}
-		else {
+		HMODULE h_ntdll = GetModuleHandle(TEXT("ntdll.dll"));
 
-			DetourTransactionBegin();
-			DetourUpdateThread(GetCurrentThread());
-			DetourAttach(&(PVOID&)true_Writefile, HookedWriteFile);
-
-			LONG lError = DetourTransactionCommit();
-			if (lError != NO_ERROR) {
-				MessageBox(HWND_DESKTOP, TEXT("Failed to attach the hook"), TEXT("Detours Error"), MB_OK);
-				return FALSE;
-			}
+		if (h_ntdll == 0) {
+			console.log("No handle found for ntdll.dll\n");
+			return FALSE;
 		}
+
+		true_NtCreateThread = (True_NtCreateThread)GetProcAddress(h_ntdll, "NtCreateThread");
+		if (true_NtCreateThread == NULL) {
+			console.log("GetProcAddress failed, aborting detours\n");
+			return FALSE;
+		}
+
+		DetourTransactionBegin();
+		DetourUpdateThread(GetCurrentThread());
+		DetourAttach(&(PVOID&)true_NtCreateThread, HookedNtCreateThread);
+		DetourAttach(&(PVOID&)trueCreateThread, HookedCreateThread);
+
+		LONG lError = DetourTransactionCommit();
+		if (lError != NO_ERROR) {
+			console.log("Detours failed to attach the hook\n");
+			return FALSE;
+		}
+
+		console.log("Hook attach successful\n");
 	}
 	break;
 
 	case DLL_PROCESS_DETACH:
+
 	{
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
-		DetourDetach(&(PVOID&)true_Writefile, HookedWriteFile);
+		DetourDetach(&(PVOID&)true_NtCreateThread, HookedNtCreateThread);
+		DetourDetach(&(PVOID&)trueCreateThread, HookedCreateThread);
 
 		LONG lError = DetourTransactionCommit();
 		if (lError != NO_ERROR) {
-			MessageBox(HWND_DESKTOP, L"Failed to detach the hook", L"Detours Error", MB_OK);
+			console.log("Detours failed to detach the hook\n");
 			return FALSE;
 		}
 	}
+
 	break;
 	}
 
 	return TRUE;
 }
-
-
